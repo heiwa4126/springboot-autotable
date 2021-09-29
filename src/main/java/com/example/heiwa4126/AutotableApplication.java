d669be58cbc5841fd820d6b98afb18cb16cfc7d3
Masato Ota <ota-masato@nexs.nec.co.jp>
2021-09-29 10:09:30 +0900

    initial

---
+++ src/main/java/com/example/heiwa4126/AutotableApplication.java
@@ -0,0 +1,13 @@
+package com.example.heiwa4126;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutotableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutotableApplication.class, args);
+	}
+
+}
